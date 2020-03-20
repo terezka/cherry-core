b7dbd9c5735dc98f73dc42d5e9760b5a6259ca89
Tereza Sokol <terezasokol@gmail.com>
2020-03-19 20:59:49 -0400

    Formatting

--- examples/Logging/src/Main.hs
+++ examples/Logging/src/Main.hs
@@ -5,8 +5,9 @@ import qualified App
 import qualified Keys
 import qualified Settings
 import qualified Logging
+import Cherry.Program (Program)
 
 
-main :: Program.Program
+main :: Program
 main =
   Program.program Settings.decoder Keys.init Logging.outputs App.app
